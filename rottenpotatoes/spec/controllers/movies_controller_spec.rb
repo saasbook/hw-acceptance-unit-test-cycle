309ff74650b2623d87f23e3d94afa1f91e1e7e1e
aclapinpepin <aclapinpepin@gmail.com>
2015-12-08 17:40:11 +0100

    Update repository

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe MoviesController do
   describe "#similar_movies" do
     it "grabs the movie from which we want to find similar movies" do
-      movie = FactoryGirl.create(:movie)
+      movie = create(:movie)
       get :similar_movies, { id: movie.id }
       expect(assigns(:movie)).to eq(movie)
     end
 
     context "when a movie has a director" do
       it "should redirect to the similar movies page" do
-        movie = FactoryGirl.create :movie
+        movie = create :movie
         get :similar_movies, { id: movie.id }
         expect(response).to render_template("similar_movies")
       end
@@ -18,7 +18,7 @@ RSpec.describe MoviesController do
 
     context "when a movie has no director" do
       it "should redirect to the home page" do
-        movie = FactoryGirl.create :movie, director: nil
+        movie = create :movie, director: nil
         get :similar_movies, { id: movie.id }
         expect(response).to redirect_to("/")
         expect(flash[:notice]).to be_present
@@ -28,13 +28,13 @@ RSpec.describe MoviesController do
 
   describe "#destroy" do
     it "grabs the movie we want to destroy" do
-      movie = FactoryGirl.create(:movie)
+      movie = create(:movie)
       get :destroy, { id: movie.id }
       expect(assigns(:movie)).to eq(movie)
     end
 
     it "deletes the movie" do
-      movie = FactoryGirl.create(:movie)
+      movie = create(:movie)
       delete :destroy, { id: movie.id }
       expect(flash.notice).to eq("Movie '#{movie.title}' deleted.")
     end
@@ -42,7 +42,7 @@ RSpec.describe MoviesController do
 
   describe "#show" do
     it "redirects to the selected movie" do
-      movie = FactoryGirl.create :movie
+      movie = create :movie
       get :show, { id: movie.id }
       expect(assigns(:movie)).to eq(movie)
       expect(response).to render_template(:show)
@@ -51,7 +51,8 @@ RSpec.describe MoviesController do
 
   describe "#create" do
     before do
-      post :create, movie: FactoryGirl.attributes_for(:movie)
+      movie_attributes = { title: "This little one", rating: "G", description: "A long boring movie", director: "Bob Lepine", release_date: "1977-02-22" }
+      post :create, movie: movie_attributes
     end
 
     it "creates a new movie" do
