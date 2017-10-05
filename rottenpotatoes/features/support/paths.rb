086444855d719bb024bab487ba54bc96114f819b
an-ju <an_ju@berkeley.edu>
2017-10-04 17:39:35 -0700

    Add some cucumber test cases and step definitions.

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -13,8 +13,7 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the home\s?page$/
-      '/'
+    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
